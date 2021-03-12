2db97921d3ba601d3cada310963933ffc2fa4f12
Charles Hall <chall8001@gmail.com>
2021-03-11 21:42:06 -0700

    shifted front end js into handlebars

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,39 +1,45 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-document.addEventListener('DOMContentLoaded', (event) => {
-    if (event) {
-      console.info('DOM loaded');
-    }
+//see index.handlebars
+
+
+
+// // Make sure we wait to attach our handlers until the DOM is fully loaded.
+// console.log("hello world")
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     if (event) {
+//       console.info('DOM loaded');
+//     }
     
-    //create burgers 
-    const createBurgerBtn = document.getElementById('burgerSubmitBtn')
-    createBurgerBtn.addEventListener("click", function() {
-      const newBurger = {
-        name: document.getElementById('ca').nodeValue.trim(),
-      }
-        location.reload();
-      });
-    });
+//     //create burgers 
+//     const createBurgerBtn = document.getElementById('burgerSubmitBtn')
+//     createBurgerBtn.addEventListener("click", function() {
+//       // const newBurger = {
+//       //   name: document.getElementById('ca').nodeValue.trim(),
+//       // }
+//       //   location.reload();
+//       console.log("hello world")
+//       });
+//     });
   
-      //update burgers to be devoured
-          fetch(`/api/cats/${id}`, {
-            method: 'PUT',
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-            },
+//       //update burgers to be devoured
+//           fetch(`/api/burgers/${id}`, {
+//             method: 'PUT',
+//             headers: {
+//               Accept: 'application/json',
+//               'Content-Type': 'application/json',
+//             },
   
-            // make sure to serialize the JSON body
-            body: JSON.stringify(newSleepState),
-          }).then((response) => {
-            // Check that the response is all good
-            // Reload the page so the user can see the new quote
-            if (response.ok) {
-              console.log(`changed sleep to: ${newSleep}`);
-              location.reload('/');
-            } else {
-              alert('something went wrong!');
-            }
-          });
+//             // make sure to serialize the JSON body
+//             body: JSON.stringify(newBurger),
+//           }).then((response) => {
+//             // Check that the response is all good
+//             // Reload the page so the user can see the new quote
+//             if (response.ok) {
+//               console.log(`changed sleep to: ${newSleep}`);
+//               location.reload('/');
+//             } else {
+//               alert('something went wrong!');
+//             }
+//           });
           
 
 
