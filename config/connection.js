d2ebbdfb69b77fce77b96f089754688881ec33e2
Charles Hall <chall8001@gmail.com>
2021-04-06 05:38:55 -0600

    added jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require('mysql')
 //create connection object 
-const connection = mysql.createConnection({
+var connection 
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+connection = mysql.createConnection({
     host: 'localhost',
     port: 3306, 
     user: 'root',
     password: 'aRlo41ba',
     database: 'burgers_db'
 })
+}
 
 //make connection
 connection.connect((err) => {
